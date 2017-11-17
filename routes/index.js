6c2052601815aefef70f8c33176cefc99c4f0147
Robert Watson <robertwwatson@live.co.uk>
2017-11-17 16:24:55 +0000

    Cleaned up stuff

--- routes/index.js
+++ routes/index.js
@@ -3,94 +3,100 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 module.exports = router;
 
-pageId=1
+pageId=1;
+
 
 /* render page with current pageId */
 router.get('/story-time', function(req, res) {
 	var db = req.db;
 	var collection = db.get('pages');
-	collection.findOne({ _id: 1 },{},function(e,docs){
-		if (docs != null) {
-			collection.findOne({ _id: pageId },{},function(e,docs){
-				res.render('story-time', {
-					"hithere" : docs
-				});
-					
-			});
+
+	collection.findOne({"_id": pageId },{},function(e,docs){
+		if (e){
+			res.send("Couldn't find page with that id");
 		}
 		else {
-			
+			res.render('story-time', {"thispage" : docs});	
 		}
-			
 	});
 });
 
-// go back to page 1 //
+
+// Go back to page 1 //
 router.post('/page1', function(req, res) {
-	pageId=1
-	res.redirect("story-time")
+	pageId=1;
+	res.redirect("story-time");
 });
 
-//*************NEEDS FINISHING FOR ALL SENTENCES******************//
-/* Create new story page*/
-router.post('/makeStory', function(req, res) {
+
+/* Create new page then go there*/
+router.post('/makePage', function(req, res) {
 	var db = req.db;
 	var collection = db.get('pages');
-	collection.count({}, {},function(e,pageTotal){ 
-		newPageId = pageTotal + 1;
-		var slField = req.body.s1ms !=null ? 's1l' : req.body.s2ms !=null ? 's2l' : req.body.s3ms !=null ? 's3l' : req.body.s4ms !=null ? 's4l' : 'something went wrong with has story sentence field selector';
-		var sField = req.body.s1ms !=null ? 's1' : req.body.s2ms !=null ? 's2' : req.body.s3ms !=null ? 's3' : req.body.s4ms !=null ? 's4' : 'something went wrong with has story sentence field selector';
-		console.log(slField)
-		console.log(sField)
-		collection.update({"_id": pageId }, {$set: {[slField] : newPageId}});
-		collection.findOne({"_id": pageId }, {[sField] :1},function(e,midDoc){
-			var middle = midDoc.s1 !=null ? midDoc.s1 : midDoc.s2 !=null ? midDoc.s2 : midDoc.s3 !=null ? midDoc.s3 : midDoc.s4 !=null ? midDoc.s4 : 'couldnt find the middle sentence'
-			console.log("middle of", newPageId, "is", middle)
-			collection.insert({"_id": newPageId, "middle": middle});
-			pageId = newPageId;
-			res.redirect("story-time");
-		});
-		
-		
-	});
 
+	collection.count({}, {},function(e,pageTotal){ 
+		if (e){
+			res.send("Couldn't count number of pages");
+		}
+		else {
+			newPageId = pageTotal + 1;
+			var nextPageLink = req.body.topMakePage != null ? 'topLink' : req.body.leftMakePage != null ? 'leftLink' : req.body.rightMakePage != null ? 'rightLink' : req.body.botMakePage != null ? 'botLink' : 'something went wrong with has story sentence field selector';
+			console.log(nextPageLink + "s to " + newPageId);
+			collection.update({"_id": pageId }, {$set: {[nextPageLink] : newPageId}});
+			
+			var sentencePosition = req.body.topMakePage != null ? 'top' : req.body.leftMakePage != null ? 'left' : req.body.rightMakePage != null ? 'right' : req.body.botMakePage != null ? 'bot' : 'something went wrong with has story sentence field selector';
+			collection.findOne({"_id": pageId }, {[sentencePosition] :1}, function(e,midDoc){
+				if (e){
+				res.send("ERROR: Couldn't find the " + sentencePosition + " for pageid " + pageId);
+				}
+				else {
+					var middle = midDoc.top != null ? midDoc.top : midDoc.left != null ? midDoc.left : midDoc.right != null ? midDoc.right : midDoc.bot != null ? midDoc.bot : 'couldnt find the middle sentence';
+					console.log("middle of page ", newPageId, "is", middle);
+					collection.insert({"_id": newPageId, "middle": middle});
+					pageId = newPageId;
+					res.redirect("story-time");
+				}
+			});
+		}
+	});
 });
+
 
 /* Go to existing storypage */
-router.post('/hasStory', function(req, res) {
+router.post('/hasPage', function(req, res) {
 	var db = req.db;
 	var collection = db.get('pages');
-	var slField = req.body.s1hs !=null ? 's1l' : req.body.s2hs !=null ? 's2l' : req.body.s3hs !=null ? 's3l' : req.body.s4hs !=null ? 's4l' : 'something went wrong with has story sentence field selector';
-	console.log(slField)
-	collection.findOne({"_id": pageId }, {[slField] :1},function(e,page){
-		console.log(page) 
-		pageId = page.s1l !=null ? page.s1l : page.s2l !=null ? page.s2l : page.s3l !=null ? page.s3l : page.s4l !=null ? page.s4l : 'no link found?'
-		res.redirect("story-time");
+	// find which sentence is clicked
+	var nextPageLink = req.body.topHasPage != null ? 'topLink' : req.body.leftHasPage != null ? 'leftLink' : req.body.rightHasPage != null ? 'rightLink' : req.body.botHasPage != null ? 'botLink' : 'something went wrong with has story sentence field selector';
+	console.log(nextPageLink);
+
+	//return link for sentence,
+	collection.findOne({"_id": pageId }, {[nextPageLink] :1}, function(e,page){
+		if (e){
+			res.send("Couldn't count number of pages");
+		}
+		else {
+			console.log(page);
+			pageId = page.topLink != null ? page.topLink : page.leftLink != null ? page.leftLink : page.rightLink != null ? page.rightLink : page.botLink != null ? page.botLink : 'no link found?';
+			res.redirect("story-time");
+		}
 	});
-	
 });
+
 
 router.post('/saveSentence', function(req, res) {
 	var db = req.db;
 	var collection = db.get('pages');
 	// find which sentence is being submitted
-	var sVal = req.body.s1 !=null ? req.body.s1 : req.body.s2 !=null ? req.body.s2 : req.body.s3 !=null ? req.body.s3 : req.body.s4 !=null ? req.body.s4 : 'something went wrong with sentence value selector';
-	var sField = req.body.s1 !=null ? 's1' : req.body.s2 !=null ? 's2' : req.body.s3 !=null ? 's3' : req.body.s4 !=null ? 's4' : 'something went wrong with sentence field selector';
+	var sentenceText = req.body.top != null ? req.body.top : req.body.left != null ? req.body.left : req.body.right != null ? req.body.right : req.body.bot != null ? req.body.bot : 'something went wrong with sentence value selector';
+	var sentencePosition = req.body.top != null ? 'top' : req.body.left != null ? 'left' : req.body.right != null ? 'right' : req.body.bot != null ? 'bot' : 'something went wrong with sentence field selector';
+	console.log(sentencePosition + " sentence is " + sentenceText);
 	// if Field not empty update
-	if (sVal != null && sVal !="" )
-			collection.update({"_id": pageId }, {$set: {[sField] : sVal}
-		}, 
-		function (err, doc) {
-		if (err) {
-			// If it failed, return error
-			res.send("There was a problem adding the information to the database.");
-		}
-		else {
-			// And forward to success page
-			res.redirect("story-time");
-		}
-	});
+	if (sentenceText != null && sentenceText != "" ){
+		collection.update({"_id": pageId }, {$set: {[sentencePosition] : sentenceText}});
+		res.redirect("story-time");
+	}
    	else {
-		res.redirect("story-time")
-	}   
+		res.send('Please do not enter a blank sentence');
+	}
 });
