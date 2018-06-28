515b665748b6ffa03812f7784778b01c7a921723
Robert Watson <robertwwatson@live.co.uk>
2018-06-28 04:17:10 +0100

    Timestamps added to console

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ pageId=1;
 
 /* render page with current pageId */
 router.get('/story-time', function(req, res) {
+		var now = new Date();
+console.log("\n" + now + "\n  Currently on page: " + pageId)
 	req.collection.findOne({"_id": pageId },{},function(e,docs){
 		if (e){
 			res.send("Couldn't find page with that id");
@@ -21,6 +23,8 @@ router.get('/story-time', function(req, res) {
 
 // Go back to page 1 //
 router.post('/page1', function(req, res) {
+	var now = new Date();
+	console.log(now + "\n  Returned to page 1.")
 	pageId=1;
 	res.redirect("story-time");
 });
@@ -28,6 +32,8 @@ router.post('/page1', function(req, res) {
 
 /* Create new page then go there*/
 router.post('/makePage', function(req, res) {
+	var now = new Date();
+	console.log("\n" + now)
 	req.collection.count({}, {},function(e,pageTotal){ 
 		if (e){
 			res.send("Couldn't count number of pages");
@@ -38,7 +44,7 @@ router.post('/makePage', function(req, res) {
 			req.collection.update({"_id": pageId }, {$set: {[nextPageLink] : newPageId}});
 			var sentencePosition = ("" + nextPageLink).slice(0,-4);
 
-			console.log("\nClicked on " + sentencePosition + " sentence on page " + pageId + " which links to the current page: " + newPageId);
+			console.log("  Clicked on " + sentencePosition + " sentence on page " + pageId + " which links to the current page: " + newPageId);
 
 			req.collection.findOne({"_id": pageId }, {[sentencePosition] :1}, function(e,midDoc){
 				if (e){
@@ -46,7 +52,7 @@ router.post('/makePage', function(req, res) {
 				}
 				else {
 					var middle = Object.values(midDoc);
-					console.log(" Middle of page ", newPageId, "is", middle[1]);
+					console.log("  Middle of page ", newPageId, "is", middle[1]);
 					req.collection.insert({"_id": newPageId, "middle": middle[1]});
 					pageId = newPageId;
 					res.redirect("story-time");
@@ -59,9 +65,11 @@ router.post('/makePage', function(req, res) {
 
 /* Go to existing storypage */
 router.post('/hasPage', function(req, res) {
+	var now = new Date();
+	console.log("\n" + now)
 	// find which sentence is clicked
 	var nextPageLink = req.body.topHasPage != null ? 'topLink' : req.body.leftHasPage != null ? 'leftLink' : req.body.rightHasPage != null ? 'rightLink' : req.body.botHasPage != null ? 'botLink' : 'Something went wrong with hasPage nextPageLink.\n';
-	console.log(nextPageLink);
+	console.log("  Clicked on " + nextPageLink);
 
 	//return link for sentence,
 	req.collection.findOne({"_id": pageId }, {[nextPageLink] :1}, function(e,page){
@@ -69,19 +77,20 @@ router.post('/hasPage', function(req, res) {
 			res.send("Couldn't count number of pages");
 		}
 		else {
-			console.log(page);
 			pageId = page.topLink != null ? page.topLink : page.leftLink != null ? page.leftLink : page.rightLink != null ? page.rightLink : page.botLink != null ? page.botLink : 'no link found?';
 			res.redirect("story-time");
 		}
 	});
 });
 
-
+//create new sentence
 router.post('/saveSentence', function(req, res) {
+	var now = new Date();
+	console.log("\n" + now)
 	// find which sentence is being submitted
 	var sentenceText = Object.values(req.body);
 	var sentencePosition = Object.keys(req.body);
-	console.log("\nCreated Sentence on page " + pageId + "\n At position: " + sentencePosition + "\n Saying: " + sentenceText);
+	console.log("  Created Sentence on page " + pageId + "\n   At position: " + sentencePosition + "\n   Saying: " + sentenceText);
 	// if Field not empty update
 	if (sentenceText != null && sentenceText != "" ){
 		req.collection.update({"_id": pageId }, {$set: {[sentencePosition] : sentenceText}});
